512864747dc007e1c3224d341b3cb9244dc2a397
kumar keshav <keshav09kr@gmail.com>
2022-08-15 04:55:28 +0000

    added postorder in binary tree

--- Binary_Tree/binaryTree.java
+++ Binary_Tree/binaryTree.java
@@ -44,12 +44,22 @@ public class binaryTree {
         inorder(root.right);
     }
 
+    public static void postorder(Node root) {
+        if(root == null) {
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.println(root.data);
+    }
+
     public static void main(String args[]) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.builtTree(nodes);
         //System.out.println(root.data);
         //preorder(root);
-        inorder(root);
+        //inorder(root);
+        postorder(root);
     }
 }
