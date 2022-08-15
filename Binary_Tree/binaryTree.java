72ec476eba42b13dee0401cc4258548dd721790f
kumar keshav <keshav09kr@gmail.com>
2022-08-15 04:51:02 +0000

    Updated bInaryTree

--- Binary_Tree/binaryTree.java
+++ Binary_Tree/binaryTree.java
@@ -34,11 +34,22 @@ public class binaryTree {
         preorder(root.left);
         preorder(root.right);
     }
+
+    public static void inorder(Node root) {
+        if(root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.println(root.data);
+        inorder(root.right);
+    }
+
     public static void main(String args[]) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.builtTree(nodes);
         //System.out.println(root.data);
-        preorder(root);
+        //preorder(root);
+        inorder(root);
     }
 }
